4f01221bd0bf50a5a7aeb45e7c0ae67a843f0686
mohamedwaelbenismail <ahmedgrati1999@gmail.com>
2020-12-05 14:09:29 +0100

    add inputs

--- simplex.py
+++ simplex.py
@@ -11,13 +11,52 @@ def hasPositiveValue(vector):
     return False
 
 
-coeff_z_array = np.array([30, 50, 0, 0, 0])
-matrix = np.array([[1, 0, 1, 0, 0, 4],
-                   [0, 2, 0, 1, 0, 12],
-                   [3, 2, 0, 0, 1, 18]
-                   ], dtype=float)
-vr_coeff_array = np.zeros(3, dtype=float)
-zj = np.zeros(5, dtype=float)
+print('Enter variables number:')
+number_of_variables = int(input())
+list = []
+# enter maximization equation
+for i in range(number_of_variables):
+    print('enter the value of the variable x',i+1,'in the maximization equation')
+    val = int(input())
+    list.append(val)
+
+# enter the number of inequalities
+print('Enter the number of inequalities: ')
+number_of_inequalities = int(input())
+list.extend(np.zeros(number_of_inequalities).tolist())
+print(list)
+# constructing our matrix
+matrix_list = []
+# e values
+e_values = np.zeros(number_of_inequalities)
+e_values[0] = 1
+for i in range(number_of_inequalities):
+    values_list = []
+    for j in range(number_of_variables):
+        print('enter the value of the variable x', j + 1, 'in the inequality ', i+1)
+        value = float(input())
+        values_list.append(value)
+
+    # avoid regenerating another np zeros
+    if i != 0:
+        e_values[i-1] = 0
+        e_values[i] = 1
+
+    for e_value in e_values:
+        values_list.append(e_value)
+    print('Enter the result of the inequality ', i+1)
+    res = float(input())
+    # construct our list
+    values_list.append(res)
+    # append our list to the matrix
+    matrix_list.append(values_list)
+print(matrix_list)
+
+
+coeff_z_array = np.array(list)
+matrix = np.array(matrix_list, dtype=float)
+vr_coeff_array = np.zeros(number_of_inequalities, dtype=float)
+zj = np.zeros(number_of_variables+number_of_inequalities, dtype=float)
 cj_minus_zj = coeff_z_array - zj
 
 # first iteration
